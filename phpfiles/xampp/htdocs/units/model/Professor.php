99816bf5f3b5b85a8d46bf4754d0c710693e8720
NiooshaDe <niooshade2001@gmail.com>
2021-09-07 19:24:02 +0330

    Professor if unique checking

--- phpfiles/xampp/htdocs/units/model/Professor.php
+++ phpfiles/xampp/htdocs/units/model/Professor.php
@@ -4,10 +4,23 @@ require_once "dataSource/DB.php";
 
 class Professor
 {
+    public $flag;
     public function addProfessor($name)
     {
         $connection = DB::connect();
 
+        $sqlNames = "SELECT name FROM professor";
+        $results = mysqli_fetch_all($connection->query($sqlNames));
+        $this->flag = 0;
+        foreach( $results as $result){
+           if($result[0] == $name){
+               echo "An error has occured";
+               $this->flag = 1;
+               die();
+           }
+       }
+
+        if($this->flag == 0){
         $sql = "INSERT INTO professor (name) VALUES ('$name')";
         $connection->query($sql);
 
@@ -15,6 +28,7 @@ class Professor
             echo "New record created successfully";
         } else {
             echo "Error!" . $connection->error;
+        }
         }
     }
 
